991fc952caed94744aeb7192e9ddad602c1f8146
Randy <randy78520@gmail.com>
2025-06-27 14:23:16 +0200

    update arduino

--- GreenIsland/plantid/arduinoviews.py
+++ GreenIsland/plantid/arduinoviews.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
+from datetime import datetime
 from .firebase import db
 
 
@@ -13,6 +14,7 @@ class ArduinoDataView(APIView):
             "pression": request.data.get('pression'),
             "sol": request.data.get('sol'),
             "niveau_eau": request.data.get('niveau_eau'),
+            "date_heure": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         }
 
         # Enregistre dans une collection Firestore (par exemple "mesures")
