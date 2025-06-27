627306b50241a32796dbbb45161a1ab2ca334079
Randy <randy78520@gmail.com>
2025-06-27 02:22:18 +0200

    update requirements

--- GreenIsland/GreenIsland/settings.py
+++ GreenIsland/GreenIsland/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-DEBUG = os.environ.get("DEBUG","False").lower() ==  "true"
+DEBUG = True
 SECRET_KEY = os.environ.get("SECRET_KEY","change-me")
 ALLOWED_HOSTS = [
     'greenislandback.onrender.com',
