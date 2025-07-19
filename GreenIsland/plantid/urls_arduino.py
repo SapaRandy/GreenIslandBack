28b15dfcccd0c2d17cfb4879976044812a6cf59a
Randy <randy78520@gmail.com>
2025-07-20 00:40:37 +0200

    update arduino

--- GreenIsland/plantid/urls_arduino.py
+++ GreenIsland/plantid/urls_arduino.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('sensor/', ArduinoDataView.as_view(), name='arduino-infos'),
     path('waterpump/', WaterPumpView.as_view(), name='water-pump'),
     path('init/', ArduinoInit.as_view(), name='init'),
+    path('connect/', ArduinoConnect.as_view(), name='init'),
 ]
