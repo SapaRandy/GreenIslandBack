e50dadff40578371309d465eefcfa12436beeced
Randy <randy78520@gmail.com>
2025-07-23 09:46:48 +0200

    update arduino

--- GreenIsland/plantid/urls.py
+++ GreenIsland/plantid/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('identify/', PlantIdentifyView.as_view(), name='identify-plant'),
     path('connect/', PlantConnectView.as_view(), name='plant-connect'),
     path('connect/<str:userId>/', PlantConnectView.as_view(), name='plant-connect'),
+    path('connect/<str:uniqueId>/<str:plantId>/', PlantConnectView.as_view(), name='plant-connect'),
     path('infos/', PlantScrapAPIView.as_view(), name='plant-info'),
     path('mesures/<str:plantId>/', PlantDataAPIView.as_view(), name='mesures'),
     path('meteo/', WeatherAPIView.as_view(), name='meteo'),
